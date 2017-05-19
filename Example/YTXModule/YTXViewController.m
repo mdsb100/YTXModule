2a9e4833d8888b6da1d08c69fb2bb26e5306a86a
Chuan <xiaochuan.xi@baidao.com>
2017-05-19 15:05:12 +0800

    升级至0.7.2，修复注释warning。

--- Example/YTXModule/YTXViewController.m
+++ Example/YTXModule/YTXViewController.m
@@ -28,8 +28,8 @@
     [YTXModule openURL:@"YTX://QUERY/query?age=18&name=CJ"];
     [YTXModule openURL:@"NOParametersAndCompletion"];
     
-    NSString * testObject1 = [YTXModule objectForURL:@"object" withUserInfo:@{@"Test":@1}];
-    NSString * testObject2 = [YTXModule objectForURL:@"object1" withUserInfo:@{@"Test":@2}];
+//    NSString * testObject1 = [YTXModule objectForURL:@"object" withUserInfo:@{@"Test":@1}];
+//    NSString * testObject2 = [YTXModule objectForURL:@"object1" withUserInfo:@{@"Test":@2}];
     
     [YTXModule openURL:@"YTX://Test"];
     
--- YTXModule.podspec
+++ YTXModule.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'YTXModule'
-  s.version          = '0.7.1'
+  s.version          = '0.7.2'
   s.summary          = 'YTXModule 组件化'
 
 # This description is used to generate tags and improve search results.
