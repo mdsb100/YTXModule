378d8430463ccdc671f820fea9f5d014a537165e
caojun <78612846@qq.com>
2017-06-16 14:28:10 +0800

    增加jump功能，希望增加test coverage和sonar

--- Example/Tests/TestYTXModuleJumpSpec.m
+++ Example/Tests/TestYTXModuleJumpSpec.m
@@ -10,6 +10,11 @@
 
 UIViewController *sign;
 
+static id testVC1;
+static id testVC2;
+static id testUserInfo;
+static id testCompletion;
+
 @interface YTXModuleJump (Test)
 
 + (void)jumpTest:(NSDictionary *)data from:(UIViewController *)viewController;
@@ -24,6 +29,35 @@ UIViewController *sign;
 
 @end
 
+@interface YTXTest : YTXModule
+
+@end
+
+@implementation YTXTest
+
+YTXMODULE_EXTERN_ROUTER_METHOD(@"YTX://SVC")
+{
+    YTXMODULE_SOURCE_VIEWCONTROLLER(vc);
+    testVC1 = vc;
+}
+
+YTXMODULE_EXTERN_ROUTER_METHOD(@"YTX://SVC/ALL")
+{
+    YTXMODULE_EXAPAND_VIEWCONTROLLER_USRINFO_COMPLETION(vc, userinfo, completion);
+    testVC2 = vc;
+    testUserInfo = userinfo;
+    testCompletion = completion;
+    completion(@15);
+}
+
+YTXMODULE_EXTERN()
+{
+    //相当于load
+    
+}
+
+@end
+
 SPEC_BEGIN(InitialTestYTXModuleJumpSpec)
 
 describe(@"测试YTXModuleJump", ^{
@@ -31,6 +65,29 @@ describe(@"测试YTXModuleJump", ^{
         UIViewController *viewController = [UIViewController new];
         [YTXModuleJump jumpPage:@"Test" data:nil from:viewController];
         [[sign should] equal:viewController];
+    });
+});
+
+describe(@"测试jumpWithUrl", ^{
+    it(@"只传vc", ^{
+        UIViewController *viewController = [UIViewController new];
+        [YTXModuleJump jumpWithUrl:@"YTX://SVC" fromViewController:viewController];
+        [[testVC1 should] equal:viewController];
+    });
+    
+    it(@"传所有参数", ^{
+        static id testResult;
+        UIViewController *viewController = [UIViewController new];
+        
+        [YTXModuleJump jumpWithUrl:@"YTX://SVC/ALL" fromViewController:viewController userInfo:@{@"test":@1} completion:^(id result) {
+            testResult = result;
+        }];
+        
+        [[testVC2 should] equal:viewController];
+        [[testUserInfo should] beNonNil];
+        [[testUserInfo[@"test"] should] equal:@1];
+        [[testCompletion should] beNonNil];
+        [[testResult should] equal:@15];
     });
 });
 
